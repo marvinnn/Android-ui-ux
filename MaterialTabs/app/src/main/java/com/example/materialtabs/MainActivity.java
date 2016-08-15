3b96b65ddddd6d550f69d49dc5bca28cab87f6f4
marvinnn <marvin12@mhs.if.its.ac.id>
2016-08-15 09:37:23 +0700

    Add fixed and scrollable tabs

---
+++ MaterialTabs/app/src/main/java/com/example/materialtabs/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.materialtabs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private Button fixedButton, scrollButton, iconTextButton, iconButton, customButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fixedButton = (Button)findViewById(R.id.fixed);
+        scrollButton = (Button)findViewById(R.id.scrollable);
+        iconTextButton = (Button)findViewById(R.id.icon_text);
+        iconButton = (Button)findViewById(R.id.icon);
+        customButton = (Button)findViewById(R.id.custom);
+
+        fixedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FixedActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        scrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScrollableActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        iconTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        iconButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
